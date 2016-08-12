27defefde33c999fe50a848403dd46cb28d14902
Elmir Jagudin <elmir.jagudin@modelon.com>
2016-08-12 13:50:06 +0200

    added 'tests' project
    Added a project for running unit/regressions tests. This
    make it possible to write and run tests with NUnit framework
    and 'Test Explorer' GUI in Visual Studio.

---
+++ tests/TestCsvFile.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using NUnit.Framework;
+using CsvCompare;
+
+namespace tests
+{
+    /// <summary>
+    /// Test CsvFile class
+    /// </summary>
+    [TestFixture]
+    public class TestCsvFile
+    {
+        const string FileName = "some.file";
+
+        /// <summary>
+        /// Test creating CsvFile object from non-exsting file.
+        /// </summary>
+        [Test]
+        public void ConstructorFileNotFound()
+        {
+            // make sure the file does not exists
+            Assert.IsFalse(File.Exists(FileName));
+
+            // check that we get appropriate exception
+            Assert.That(() => new CsvFile(FileName, new Options(), new Log()),
+                        Throws.TypeOf<FileNotFoundException>());
+        }
+    }
+}
